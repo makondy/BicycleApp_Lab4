60a36373bb9dfc979a28e6c461a01c897f336d09
Maks Kond <lyubar06m@gmail.com>
2024-10-19 13:04:24 +0300

    Add project files.

---
+++ BicycleApp_Lab4/fMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BicycleApp_Lab4
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.gvBicycles = new System.Windows.Forms.DataGridView();
+            this.bindSrcBicycles = new System.Windows.Forms.BindingSource(this.components);
+            this.btnAdd = new System.Windows.Forms.ToolStripButton();
+            this.btnEdit = new System.Windows.Forms.ToolStripButton();
+            this.btnDel = new System.Windows.Forms.ToolStripButton();
+            this.btnClear = new System.Windows.Forms.ToolStripButton();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.bicycleBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.gvBicycles)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindSrcBicycles)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bicycleBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvBicycles
+            // 
+            this.gvBicycles.AllowUserToAddRows = false;
+            this.gvBicycles.AllowUserToDeleteRows = false;
+            this.gvBicycles.AutoGenerateColumns = false;
+            this.gvBicycles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvBicycles.DataSource = this.bindSrcBicycles;
+            this.gvBicycles.Location = new System.Drawing.Point(0, 28);
+            this.gvBicycles.Name = "gvBicycles";
+            this.gvBicycles.ReadOnly = true;
+            this.gvBicycles.Size = new System.Drawing.Size(943, 409);
+            this.gvBicycles.TabIndex = 5;
+            this.gvBicycles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvBicycles_CellContentClick);
+            // 
+            // bindSrcBicycles
+            // 
+            this.bindSrcBicycles.CurrentChanged += new System.EventHandler(this.bindSrcBicycles_CurrentChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(23, 22);
+            this.btnAdd.Text = "Додати запис про велосипед";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnEdit.Image = ((System.Drawing.Image)(resources.GetObject("btnEdit.Image")));
+            this.btnEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(23, 22);
+            this.btnEdit.Text = "Редагувати запис";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnDel.Image = ((System.Drawing.Image)(resources.GetObject("btnDel.Image")));
+            this.btnDel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(23, 22);
+            this.btnDel.Text = "Видалити запис";
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(23, 22);
+            this.btnClear.Text = "Очистити дані";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(23, 22);
+            this.btnExit.Text = "Вийти з програми";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAdd,
+            this.btnEdit,
+            this.btnDel,
+            this.btnClear,
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(930, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.Click += new System.EventHandler(this.bindSrcBicycles_CurrentChanged);
+            // 
+            // bicycleBindingSource
+            // 
+            this.bicycleBindingSource.DataSource = typeof(BicycleApp_Lab4.Bicycle);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 449);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.gvBicycles);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №4";
+            ((System.ComponentModel.ISupportInitialize)(this.gvBicycles)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindSrcBicycles)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bicycleBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView gvBicycles;
+        private System.Windows.Forms.BindingSource bicycleBindingSource;
+        private System.Windows.Forms.ToolStripButton btnAdd;
+        private System.Windows.Forms.ToolStripButton btnEdit;
+        private System.Windows.Forms.ToolStripButton btnDel;
+        private System.Windows.Forms.ToolStripButton btnClear;
+        private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.BindingSource bindSrcBicycles;
+    }
+}
+
---
+++ BicycleApp_Lab4/fMain.cs
@@ -0,0 +1,146 @@
+﻿using BicycleApp_Lab4;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace BicycleApp_Lab4
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+            this.Load += fMain_Load; // Переконайтеся, що подія Load прив'язана
+        }
+
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            gvBicycles.AutoGenerateColumns = false;
+
+            // Створюємо і додаємо колонки
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Brand";
+            column.Name = "Марка";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Model";
+            column.Name = "Модель";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Type";
+            column.Name = "Тип";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Weight";
+            column.Name = "Вага (кг)";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Mileage";
+            column.Name = "Пробіг (км)";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "GearCount";
+            column.Name = "Кількість передач";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Distance";
+            column.Name = "Відстань (км)";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Time";
+            column.Name = "Час (год)";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Speed";
+            column.Name = "Швидкість (км/год)";
+            gvBicycles.Columns.Add(column);
+
+            // Прив'язуємо BindingSource до DataGridView
+            gvBicycles.DataSource = bindSrcBicycles;
+        }
+
+        
+
+        private void gvBicycles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void bindSrcBicycles_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            fBicycle bicycleForm = new fBicycle();
+
+            // Відображаємо форму як модальне вікно
+            if (bicycleForm.ShowDialog() == DialogResult.OK)
+            {
+                // Отримуємо новий велосипед з форми
+                Bicycle newBicycle = (Bicycle)bicycleForm.Tag;
+
+                // Додаємо новий велосипед у BindingSource
+                bindSrcBicycles.Add(newBicycle);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (gvBicycles.CurrentRow != null)
+            {
+                Bicycle selectedBicycle = (Bicycle)bindSrcBicycles.Current;
+                fBicycle bicycleForm = new fBicycle(selectedBicycle);
+
+                if (bicycleForm.ShowDialog() == DialogResult.OK)
+                {
+                    bindSrcBicycles[bindSrcBicycles.Position] = bicycleForm.Tag;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, виберіть велосипед для редагування.");
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (gvBicycles.CurrentRow != null)
+            {
+                bindSrcBicycles.RemoveCurrent();
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, виберіть велосипед для видалення.");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            bindSrcBicycles.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
